d2da29e34c2909485ad9f5d6f93866e3a813cde4
malvasan <malvasan@hotmail.com>
2017-04-20 23:21:16 -0500

    Onceavo ejercicio

---
+++ multiplicacion.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+/*
+using namespace std;
+
+void multiplicacion(int arreglo1[][10], int arreglo2[][10],int fi1,int fi2,int col1,int col2)
+{
+    if(col1!=fi2)
+        return
+        ;
+    int arreglo3[fi1][col2];
+    for(int j=0;j<fi1;++j){
+        for(int i=0;i<col2;i++)
+            arreglo3[j][i]=0;
+    }
+    int i,j,k;
+    for(i=0;i<fi1;++i){
+        for(j=0;j<col2;++j){
+            for(k=0;k<fi2;++k)
+                arreglo3[i][j]+=(arreglo1[i][k])*(arreglo2[k][j]);
+        }
+    }
+    for(int j=0;j<fi1;++j){
+        for(int i=0;i<col2;i++)
+            cout<<arreglo3[j][i]<<" ";
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n,m,o,p;
+    cout<<"Ingresa el tamano de filas de la matriz 1:";
+    cin>>n;
+    cout<<"Ingresa el tamano de columnas de la matriz 1:";
+    cin>>m;
+    cout<<"Ingresa el tamano de filas de la matriz 2:";
+    cin>>o;
+    cout<<"Ingresa el tamano de columnas de la matriz 2:";
+    cin>>p;
+    cout<<"Ingresa los terminos"<<endl;
+    int arreglo1[n][10];
+    int arreglo2[o][10];
+    for(int j=0;j<n;++j){
+        for(int i=0;i<m;i++)
+            cin>>arreglo1[j][i];
+    }
+    cout<<endl;
+    for(int j=0;j<o;++j){
+        for(int i=0;i<p;i++)
+            cin>>arreglo2[j][i];
+    }
+    multiplicacion(arreglo1,arreglo2,n,o,m,p);
+}
+*/
