ce116de9730dd7abe94cebd81bc5435c3fdaaef7
malvasan <malvasan@hotmail.com>
2017-04-20 23:16:12 -0500

    Tercer ejercicio

---
+++ par_o_impar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+/*
+using namespace std;
+
+void par_impar(int x)
+{
+    if(x%2==0)
+        cout<<x<<" es par"<<endl;
+    else
+        cout<<x<<" es impar"<<endl;
+    cout<<"Para terminar poner -1"<<endl;
+}
+int main()
+{
+    bool seguir=true;
+    while(seguir==true){
+        int y;
+        cout<<"Que numero deseas saber ";
+        cin>>y;
+        if(y==-1)
+            seguir=false;
+        else
+            par_impar(y);
+    }
+}
+*/
