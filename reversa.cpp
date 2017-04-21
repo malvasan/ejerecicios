0277413cd17cdb39f5938dd054c341cc1c20726f
malvasan <malvasan@hotmail.com>
2017-04-20 23:21:51 -0500

    Doceavo ejercicio

---
+++ reversa.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+/*
+using namespace std;
+
+void cambio(int x[], int tamano)
+{
+    tamano--;
+    int temp=0;
+    for(int i=0;i<(tamano/2);++i){
+        temp=x[i];
+        x[i]=x[tamano-i];
+        x[tamano-i]=temp;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Ingresa el tamano:";
+    cin>>n;
+    cout<<"Ingresa los"<<" "<<n<<" "<<"terminos"<<endl;
+    int arreglo[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arreglo[i];
+
+    cambio(arreglo,n);
+
+    for(int i=0;i<n;i++)
+        cout<<arreglo[i];
+}
+*/
