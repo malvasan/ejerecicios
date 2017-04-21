d87c6b1f9d16674b48b0e653188cbd5e2a881044
malvasan <malvasan@hotmail.com>
2017-04-20 23:22:24 -0500

    Treceavo ejercicio

---
+++ reversa_punt.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+/*
+using namespace std;
+
+void cambio(int *x, int tamano,int *y)
+{
+    int temp=0;
+    for(int i=0;i<(tamano/2);++i){
+        temp=*x;
+        *x=*y;
+        *y=temp;
+        ++x;
+        --y;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Ingresa el tamano:";
+    cin>>n;
+    cout<<"Ingresa los"<<" "<<n<<" "<<"terminos"<<endl;
+    int arreglo[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arreglo[i];
+
+    cambio(arreglo,n,&arreglo[n-1]);
+
+    for(int i=0;i<n;i++)
+        cout<<arreglo[i];
+}
+*/
