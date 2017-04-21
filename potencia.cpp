58c27e7e47fb87d267878a14719c0bf445c2359d
malvasan <malvasan@hotmail.com>
2017-04-20 23:16:48 -0500

    Cuarto ejercicio

---
+++ potencia.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/*
+using namespace std;
+
+int potencia(int base, int exponente)
+{
+    int resultado=1;
+    for(int i=0;i<exponente;++i){
+        resultado=resultado*base;
+    }
+    return resultado;
+}
+
+int main()
+{
+    int x,y;
+    cout<<"Ingrese el numero: ";
+    cin>>x;
+    cout<<"Igrese el exponente; ";
+    cin>>y;
+    cout<<"La potencia de "<<x<<" es:"<<potencia(x,y);
+}
+*/
