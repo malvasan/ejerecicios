b4b0a09d3720a456dae4f912f2557a69f0211b37
malvasan <malvasan@hotmail.com>
2017-04-20 23:18:41 -0500

    Septimo ejercicio

---
+++ porrefer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/*
+using namespace std;
+
+void cambio(int &x, int &y)
+{
+    int temporal;
+    temporal=x;
+    x=y;
+    y=temporal;
+}
+int main()
+{
+    int x,y;
+    cout<<"Ingrese x: ";
+    cin>>x;
+    cout<<"Ingrese y: ";
+    cin>>y;
+    cambio(x,y);
+    cout<<"Ahora x es: "<<x<<" Y y es: "<<y<<endl;
+}
+*/
