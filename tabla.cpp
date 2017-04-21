bded271078e527fd469ef4c35cad3df4afc5b148
malvasan <malvasan@hotmail.com>
2017-04-20 23:15:31 -0500

    Segundo ejercicio

---
+++ tabla.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*
+using namespace std;
+
+void imprimir()
+{
+    cout<<"Fahrenheit   "<<"   Celsius"<<endl;
+    float Fah,celsius;
+
+    for(int i=0;i<=300;i=i+20){
+        Fah=i;
+        celsius=((Fah-32)*5)/9;
+        cout<<Fah<<"--------------"<<celsius<<endl;
+
+    }
+}
+int main()
+{
+    imprimir();
+}
+*/
