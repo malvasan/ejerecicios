d5ff62c15d9ae42515cda7014e540bff88df1930
malvasan <malvasan@hotmail.com>
2017-04-20 23:14:23 -0500

    Primer ejercicio

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+using namespace std;
+
+float convertir(int mill)
+{
+    return (mill*1.60934);
+}
+int main()
+{
+    float millas;
+    cout << "Ingresa el tamano en millas :";
+    cin>>millas;
+    cout<<"El valor "<<millas<<" en millas es "<<convertir(millas)<<endl;
+    return 0;
+}
+*/
+
